7ddb457681a4d0967c2e505c26d4d669fac83953
Muhsan Javed <Muhsantech048@outlook.com>
2023-08-23 10:15:15 +0500

    add splash Screen Make

--- app/src/main/java/com/muhsanjaved/weatherapplication/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/muhsanjaved/weatherapplication/ui/SplashScreenActivity.kt
@@ -3,15 +3,18 @@ package com.muhsanjaved.weatherapplication.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.muhsanjaved.weatherapplication.R
-import com.muhsanjaved.weatherapplication.databinding.ActivityMainBinding
+import com.muhsanjaved.weatherapplication.databinding.ActivitySplashScreenBinding
 
-class MainActivity : AppCompatActivity() {
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
     }
 }
